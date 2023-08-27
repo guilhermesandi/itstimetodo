25142a07736befb24e885ecb3da50813bb9ba4a9
Guilherme Sandi <guilherme_sandi@hotmail.com>
2023-08-27 20:47:04 -0300

    refactor(checkbox): allow to move item clicking in text

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,5 +1,6 @@
 import { Check } from "lucide-react";
 import * as CheckBoxRadix from "@radix-ui/react-checkbox";
+import clsx from "clsx";
 
 interface Props {
   title: string;
@@ -9,21 +10,26 @@ interface Props {
 
 export function Checkbox({ title, isChecked, onCheckedChange }: Props) {
   return (
-    <CheckBoxRadix.Root
-      onCheckedChange={onCheckedChange}
-      checked={isChecked}
-      disabled={false}
-      className="contents items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
-    >
-      <div className="h-8 w-8 rounded-md flex flex-none items-center justify-center border-2 border-white group-data-[state=checked]:bg-primary group-data-[state=checked]:border-primary transition-colors group-focus:outline-none group-focus:ring-2 group-focus:ring-primary group-focus:ring-offset-2 group-focus:ring-offset-background mr-3">
-        <CheckBoxRadix.Indicator>
-          <Check size={20} className="text-white" />
-        </CheckBoxRadix.Indicator>
-      </div>
+    <div className="contents items-center gap-3 group focus:outline-none disabled:cursor-not-allowed">
+      <CheckBoxRadix.Root
+        onCheckedChange={onCheckedChange}
+        checked={isChecked}
+        disabled={false}
+        className="contents items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
+      >
+        <div className="h-8 w-8 rounded-md flex flex-none items-center justify-center border-2 border-white group-data-[state=checked]:bg-primary group-data-[state=checked]:border-primary transition-colors group-focus:outline-none group-focus:ring-2 group-focus:ring-primary group-focus:ring-offset-2 group-focus:ring-offset-background mr-3">
+          <CheckBoxRadix.Indicator>
+            <Check size={20} className="text-white" />
+          </CheckBoxRadix.Indicator>
+        </div>
 
-      <span className="font-semibold text-xl text-white group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400 whitespace-nowrap overflow-hidden text-ellipsis mr-2">
+      </CheckBoxRadix.Root>
+
+      <span className={clsx("font-semibold text-xl text-white whitespace-nowrap overflow-hidden text-ellipsis mr-2", {
+        'line-through text-zinc-400' : isChecked,
+      })}>
         {title}
       </span>
-    </CheckBoxRadix.Root>
+    </div>
   )
 }
